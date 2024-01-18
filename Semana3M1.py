0c452afa4a66a1c1afee7bbfcf110b2719d3e521
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-01-17 21:03:58 -0300

    Add files via upload

---
+++ Semana3M1.py
@@ -0,0 +1,59 @@
+#Mensagens iniciais simples com hashtag
+''''
+Mensagem longa com 3 aspas simples
+Sem limites de linhas
+'''
+print('Olá mundo!')
+print('Olá, segunda mensagem')
+
+"""
+Inteiro
+
+numero = in(input('Digite seu numero: '))
+print(numero) """
+
+"""
+Float
+numero = float (input ('Digite um número float: '))
+print(numero)
+"""
+
+"""
+String
+só colocar direto, não precisa converter para str porque em Python é automático
+
+frase = input ('Digite sua frase: ')
+print(frase)
+"""
+
+"""
+Operações Matemáticas
+Soma = +
+Subtração = -
+Multipicação = *
+Divisão = /
+Divisão inteira = //
+
+numero1 = int (input('Digite o número 01: '))
+numero2 = int (input('Digite o numero 02: '))
+soma =  numero1 + numero2
+print(soma)
+
+subtracao = numero1 - numero2
+print(subtracao)
+
+multiplicacao = numero1 * numero2
+print(multiplicacao)
+
+divisao = numero1 / numero2
+print(divisao)
+
+divinteira = numero1 // numero2
+print (divinteira)
+
+"""
+
+
+
+
+
